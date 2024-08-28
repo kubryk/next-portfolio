2d4679d6f8276e423fceb2c2a848741aff2a75e5
kubryk <flock1715@gmail.com>
2024-08-28 14:10:33 +0300

    Hero fix
    Hero section style fix

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -12,7 +12,7 @@ const Hero = () => {
     const sections = useContext(NavigationContext);
     return (
         <section ref={sections?.find(section => section.name === 'About')?.ref} className="h-screen p">
-            <Container className="flex flex-col items-center justify-evenly w-full md:flex-row md:gap-5">
+            <Container className="flex flex-col items-center pt-20 w-full md:flex-row md:gap-5 md:justify-evenly">
 
                 <div className="flex flex-col gap-3 p-3">
                     <h1 className="xsm:text-center sm:text-center md:text-left text-[50px] font-extrabold xsm:text-4xl sm:text-5xl md:text-5xl lg:text-6xl">Hi, it&apos;s <span className=" text-indigo-500">Yaroslav</span></h1>
@@ -27,17 +27,20 @@ const Hero = () => {
                     </Button>
                 </div>
 
-                <div className="relative ">
+                <Image src="/my-photo.png" alt="My Photo" width={300} height={300} />
+
+
+                {/* <div className="relative ">
                     <svg className=" w-[340px] h-[340px]">
                         <path className=" fill-transparent " id="curve" d=" M 320 170 A 150 150 0 1 1 320 165" />
                         <text className="origin-center animate-spin duration-10000 transition-colors">
                             <textPath className="dark:fill-white" href="#curve">
-                                | HTML | CSS | SCSS | JavaScript | Typescript | React | Next.js | Node.js | Express | MongoDB | PostgreSQL | Redux | Tailwind | SEO |
+                                HTML  CSS  SCSS  JavaScript  Typescript  React  Next.js  Node.js  Express  MongoDB  PostgreSQL  Redux  Tailwind  SEO
                             </textPath>
                         </text>
                     </svg>
                     <Image className="absolute top-[30px] left-[30px]" src="/my-photo.png" alt="My Photo" width={280} height={280} />
-                </div>
+                </div> */}
 
             </Container>
         </section>
