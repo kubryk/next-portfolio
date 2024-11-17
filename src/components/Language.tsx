1b8b48b4202fba75665bb8b8d1de1f3d0763fe59
kubryk <flock1715@gmail.com>
2024-11-17 19:51:59 +0200

    lang toggler

--- src/components/Language.tsx
+++ src/components/Language.tsx
@@ -1,100 +1,35 @@
 "use client"
 import * as React from "react"
-// import ReactCountryFlag from "react-country-flag"
-// import { Button } from "./ui/button"
-// import {
-//     Command,
-//     CommandEmpty,
-//     CommandGroup,
-//     CommandInput,
-//     CommandItem,
-//     CommandList,
-// } from "@/components/ui/command"
-import {
-    Popover,
-    PopoverContent,
-    PopoverTrigger,
-} from "@/components/ui/popover"
-// import { useTranslations } from "next-intl"
-
-
-
-const languages = [
-    {
-        value: "us",
-        label: "English",
-    },
-    {
-        value: "uk-UA",
-        label: "Ukrainian",
-    },
-    {
-        value: "es",
-        label: "Spanish",
-    },
-]
+import ReactCountryFlag from "react-country-flag"
+import { useRouter } from "next/navigation"
+import { useLocale } from "next-intl"
 
 export function Language() {
-    const [open, setOpen] = React.useState(false)
-    const [value, setValue] = React.useState("")
+    const router = useRouter();
+    const localeActive = useLocale();
+    const [transition, startTransition] = React.useTransition();
 
+
+    const onSelectChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+        const nextLocale = e.target.value;
+        startTransition(() => {
+            router.replace(`/${nextLocale}`)
+        })
+    }
 
 
     return (
-        <Popover open={open} onOpenChange={setOpen}>
-            {/* <PopoverTrigger asChild>
-                <Button
-                    variant="outline"
-                    role="combobox"
-                    aria-expanded={open}
-                    className="w-fit justify-between p-3"
-                >
-
-                    {locale &&
-                        <ReactCountryFlag
-                            countryCode={locale}
-                            svg
-                            style={{
-                                width: '2em',
-                                height: '2em',
-                            }}
-                        />}
-                </Button>
-            </PopoverTrigger>
-
-            <PopoverContent className="w-fit p-0">
-                <Command>
-                    <CommandList>
-                        <CommandGroup>
-                            {languages.map((language) => {
-                                if (language.value != locale) {
-                                    return (
-                                        <CommandItem
-                                            key={language.value}
-                                            value={language.value}
-                                            onSelect={(currentValue) => {
-                                                // setValue(currentValue === value ? "" : currentValue)
-                                                setOpen(false)
-                                                setLocale(currentValue)
-                                            }}
-                                        >
-                                            <ReactCountryFlag
-                                                countryCode={language.value}
-                                                svg
-                                                style={{
-                                                    width: '2em',
-                                                    height: '2em',
-                                                }}
-                                                title={language.label}
-                                            />
-                                        </CommandItem>
-                                    )
-                                }
-                            })}
-                        </CommandGroup>
-                    </CommandList>
-                </Command>
-            </PopoverContent> */}
-        </Popover>
+        <div>
+            <select
+                className="bg-transparent border-none h-10 w-10"
+                value={localeActive}
+                onChange={(e) => onSelectChange(e)}
+            >
+                <option value="en"><ReactCountryFlag countryCode="US" /></option>
+                <option value="uk"><ReactCountryFlag countryCode="UA" /></option>
+                <option value="es"><ReactCountryFlag countryCode="ES" /></option>
+                <option value="de"><ReactCountryFlag countryCode="DE" /></option>
+            </select>
+        </div>
     )
 }
